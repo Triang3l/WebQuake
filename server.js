177d7f92ffa66a13e506d4e3bc32b512c015acdc
Soldy <4786022+Soldy@users.noreply.github.com>
2021-05-12 09:49:31 +0100

    move server.js to the main
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,31 @@
+Node = {
+	dgram: require('dgram'),
+	fs: require('fs'),
+	http: require('http'),
+	os: require('os'),
+	url: require('url'),
+	websocket: require('websocket')
+};
+require('./Server/WebQDS/Cmd.js');
+require('./Server/WebQDS/COM.js');
+require('./Server/WebQDS/Console.js');
+require('./Server/WebQDS/CRC.js');
+require('./Server/WebQDS/Cvar.js');
+require('./Server/WebQDS/Def.js');
+require('./Server/WebQDS/ED.js');
+require('./Server/WebQDS/Host.js');
+require('./Server/WebQDS/Mod.js');
+require('./Server/WebQDS/MSG.js');
+require('./Server/WebQDS/NET.js');
+require('./Server/WebQDS/NET_Datagram.js');
+require('./Server/WebQDS/NET_WEBS.js');
+require('./Server/WebQDS/PF.js');
+require('./Server/WebQDS/PR.js');
+require('./Server/WebQDS/Protocol.js');
+require('./Server/WebQDS/Q.js');
+require('./Server/WebQDS/SV.js');
+require('./Server/WebQDS/Sys.js');
+require('./Server/WebQDS/SZ.js');
+require('./Server/WebQDS/V.js');
+require('./Server/WebQDS/Vec.js');
+Sys.main();
